ab5a48acfda41a0c80697c590d682c458de4f334
Sylvester Mabasa <sp.mabasa@gmail.com>
2017-08-28 10:39:59 +0200

    all tests done

--- test/parksTest.js
+++ test/parksTest.js
@@ -12,8 +12,8 @@ chai.use(chaiHttp);
 describe('Parks', () => {
 
 	const park = {
-			name : "Park 50",
-			location : "location 50",
+			name : 'Park 50',
+			location : 'location 50',
 			entryfee: 50,
 			services : ["s 50", "s 55"]
 		}
@@ -30,6 +30,7 @@ describe('/GET parks', () => {
 		.get('/parks')
 		.end ((err, res) =>{
 			res.should.have.status(200);
+			res.should.be.json;
 			res.body.should.be.a('array');
 			done();
 		});
@@ -37,23 +38,28 @@ describe('/GET parks', () => {
 });
 
 
-// describe('/GET/:id parks', ()=>{
-// 	it('It should GET a park given the id', (done) =>{
+describe('/GET/:id parks', ()=>{
+	it('It should GET a park given the id', (done) =>{
 		
-// 		const prk = new Park();
-// 		prk.save((err, park) =>{
-// 			chai.request(server)
-// 			.get('/parks/'+park.id)
-// 			.send(park)
-// 			.end((err, res)=>{
-// 				res.should.have.status(200);
-// 				res.body.should.be.a('object');
-	
-// 				done();
-// 			});
-// 		});
-// 	});
-// });
+		const newPark = new Park(park);
+			newPark.save((err, park) =>{
+			chai.request(server)
+			.get('/parks/'+park.id)
+			.end((err, res)=>{
+				res.should.have.status(200);
+				res.should.be.json;
+				res.body.should.be.a('object');
+				res.body.should.have.property('_id');
+				res.body.should.have.property('name');
+				res.body.should.have.property('location');
+				res.body.should.have.property('entryfee');
+				res.body.should.have.property('services');
+				res.body._id.should.equal(park.id);
+				done();
+			});
+		});
+	});
+});
 
 
 
@@ -67,24 +73,68 @@ describe('/POST parks', () => {
 		.send(park)
 		.end((err, res) =>{
 			res.should.have.status(200);
+			res.should.be.json;
 			res.body.should.be.a('object');
 
-			park.should.be.a('object');
-			park.should.have.property('name');
-			park.name.should.be.a('string');
-			park.should.have.property('location');
-			park.location.should.be.a('string');
-			park.should.have.property('entryfee');
-			park.entryfee.should.be.a('number');
-			park.should.have.property('services');
-			park.services.should.be.a('array');
+			res.body.should.have.property('_id');
+			res.body.should.have.property('name');
+			res.body.name.should.be.a('string');
+			res.body.should.have.property('location');
+			res.body.location.should.be.a('string');
+			res.body.should.have.property('entryfee');
+			res.body.entryfee.should.be.a('number');
+			res.body.should.have.property('services');
+			res.body.services.should.be.a('array');
+
 			done();
 		});
 	});
 });
 
+describe('/PUT/:id parks', () =>{
 
+	it('It should update a park', (done) =>{
 
+		const newPark = new Park(park);
+			newPark.save((err, park) =>{
+
+			chai.request(server)
+			.put('/parks/'+park.id)
+			.send({ name : 'new name'})
+			.end((err, res) => {
+				res.should.have.status(200);
+				res.should.be.json;
+				res.body.should.be.a('object');
+				res.body.should.have.property('name');
+				res.body.name.should.equal('new name');
+				done();
+
+			});
+		
+		});
+	});
+});
+
+describe('/DELETE/:id parks', () =>{
+	it('It should DELETE a park', (done)=>{
+
+		const newPark = new Park(park);
+			newPark.save((err, park) =>{
+
+			chai.request(server)
+			.delete('/parks/'+park.id)
+			.end((err, res) => {
+				res.should.have.status(200);
+				res.should.be.json;
+				res.body.should.be.a('object');
+				res.body.should.have.property('message').eql('Park successfully deleted');
+							done();
+
+			});
+		
+		});
+	});
+} );
 
 
 
